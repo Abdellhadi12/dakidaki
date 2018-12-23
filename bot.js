1dba571fc25785e74b6d51a323dac06c9cccc2c5
Abdellhadi12 <44175418+Abdellhadi12@users.noreply.github.com>
2018-12-24 00:20:34 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -58,7 +58,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`!help | By hassan_games`,"http://twitch.tv/S-F")
+client.user.setGame(`!help | By Abdellhadi`,"http://twitch.tv/S-F")
 client.user.setStatus("dnd")
 });
 
