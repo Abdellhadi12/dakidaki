7d356a3c4b3f69913b9b52114c5a2429f4179292
Abdellhadi12 <44175418+Abdellhadi12@users.noreply.github.com>
2018-12-24 14:33:53 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -834,37 +834,6 @@ client.on('message', async message => {
 
 
 
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
-client.on('ready', () => {
-  console.log('I am ready!');
-});
-client.on('message',async message => {//Toxic Code
-  if(message.author.bot || message.channel.type === '!bct') return;
-  let args = message.content.split(' ');
-  if(args[0] === `!bct`) {//Toxic Code
-    if(!message.member.hasPermission("MANAGE_GUILD")) return message.channel.send('- **أنت لا تملك الصلاحيات اللازمة لأستخدام هذا الأمر**');
-    if(!args[1]) return message.channel.send('- **يجب عليك كتابة الرسالة بعد الأمر**');
- 
-    let msgCount = 0;
-    let errorCount = 0;
-    let successCount = 0;
-    message.channel.send(`**- [ :bookmark: :: ${msgCount} ] ・عدد الرسائل المرسلة**\n**- [ :inbox_tray: :: ${successCount} ] ・عدد الرسائل المستلمة**\n**- [ :outbox_tray: :: ${errorCount} ]・عدد الرسائل الغير مستلمة**`).then(msg => {//Toxic Code
-      message.guild.members.forEach(g => {//Toxic Code
-        g.send(args.slice(1).join(' ')).then(() => {//Toxic Code
-          successCount++;
-          msgCount++;
-          msg.edit(`**- [ :bookmark: :: ${msgCount} ] ・عدد الرسائل المرسلة**\n**- [ :inbox_tray: :: ${successCount} ] ・عدد الرسائل المستلمة**\n**- [ :outbox_tray: :: ${errorCount} ]・عدد الرسائل الغير مستلمة**`);
-        }).catch(e => {
-          errorCount++;
-          msgCount++;
-          msg.edit(`**- [ :bookmark: :: ${msgCount} ] ・عدد الرسائل المرسلة**\n**- [ :inbox_tray: :: ${successCount} ] ・عدد الرسائل المستلمة**\n**- [ :outbox_tray: :: ${errorCount} ]・عدد الرسائل الغير مستلمة**`);//Toxic Code
-        });//Toxic Code
-      });//Toxic Code
-    });//Toxic Code
-  }
-});//Toxic Code
 
 
 
