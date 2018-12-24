107ffadedde032e3d2305c668aa7ad810a66ca62
Abdellhadi12 <44175418+Abdellhadi12@users.noreply.github.com>
2018-12-24 16:03:18 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -838,16 +838,6 @@ client.on('message', async message => {
 
 
 
-client.on("guildMemberAdd", member => {
-  member.createDM().then(function (channel) {
-  return channel.send(`:rose:  ولكم نورت السيرفر:rose: 
-:crown: ${member}:crown:  
-انت العضو رقم ${member.guild.memberCount} `) 
-}).catch(console.error)
-})
-
-
-
 
 
 
